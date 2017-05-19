0260b010fd499f59d9dc52d4dbd3721c23193f1d
kolszer <k.olszer@gmail.com>
2017-05-19 23:26:23 +0200

    Add projects (BitonicTSP, KCenter)

---
+++ BitonicTSP/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitonicTSP
+{
+    public struct Point
+    {
+        public double X, Y;
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public override string ToString()
+        {
+            return (String.Format("({0},{1})", X, Y));
+        }
+    }
+
+    class Program
+    {
+        public static List<double> bTSP(List<Point> p)
+        {
+            double[] b = new double[p.Count];
+            double min, sum, d;
+            b[1] = dist(p[0], p[1]);
+            for(int j=2;j<p.Count;j++)
+            {
+                min = double.PositiveInfinity;
+                sum = 0;
+                for(int i=j-1;i>0;i--)
+                {
+                    d = b[i + 1] + dist(p[i], p[j]) + sum;
+                    if (d < min)
+                        min = d;
+                    sum = sum + dist(p[i], p[i + 1]);
+                }
+                b[j] = min;
+            }
+            return b.ToList();
+        }
+
+        public static double dist(Point p1, Point p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
+        }
+
+        static void Main(string[] args)
+        {
+            List<Point> pkt = new List<Point>() { new Point(1, 1), new Point(8,8), new Point(4,4), new Point(2,2) };
+            List<double> res = bTSP(pkt);
+            res.ForEach(p => Console.WriteLine(p));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ KCenter/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCenter
+{
+    public struct Point
+    {
+        public double X, Y;
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public override string ToString()
+        {
+            return (String.Format("({0},{1})", X, Y));
+        }
+    }
+
+    class Program
+    {
+        static List<Point> kCenter(List<Point> p, int k)
+        {
+            Point[] s = new Point[k];
+            double[] d = new double[p.Count];
+            double[] r = new double[p.Count];
+
+            for (int i = 0; i < p.Count; i++) d[i] = double.PositiveInfinity;
+
+            s[0] = p[0];
+
+            for (int j = 0; j < k; j++)
+            {
+                r[j] = 0;
+                for (int i = 0; i < p.Count; i++)
+                {
+                    d[i] = Math.Min(d[i], dist(p[i], s[j]));
+                    if (r[j] < d[i] && j + 1 < k)
+                    {
+                        r[j] = d[i];
+                        s[j + 1] = p[i];
+                    }
+                }
+            }
+
+            return s.ToList();
+        }
+
+        public static double dist(Point p1, Point p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
+        }
+
+        static void Main(string[] args)
+        {
+            List<Point> pkt = new List<Point>() { new Point(9, 9), new Point(-1, -1), new Point(11, 11), new Point(10, 10), new Point(0, 0), new Point(1, 1), new Point(100, 100) };
+            List<Point> res = kCenter(pkt, 3);
+            res.ForEach(p => Console.WriteLine(p.ToString()));
+
+            Console.ReadKey();
+        }
+    }
+}
